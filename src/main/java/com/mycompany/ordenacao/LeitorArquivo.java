0670a54f97deae8dbd3cb4c55663aa9a3d23e8bc
Manuela Bertella Ossanes <manuelaossanes@hotmail.com>
2023-10-30 21:38:24 -0300

    Merge origin/master
    Conflicts:
    	src/LivrosSemaforo/Livros.java
    	src/LivrosSemaforo/Main.java
    	src/LivrosSemaforo/Usuario.java
    	src/LivrosSynchronized/Livros.java
    	src/LivrosSynchronized/Main.java
    	src/LivrosSynchronized/Usuario.java

--- src/main/java/com/mycompany/ordenacao/LeitorArquivo.java
+++ src/main/java/com/mycompany/ordenacao/LeitorArquivo.java
@@ -1,4 +1,5 @@
 package com.mycompany.ordenacao;
+
 import org.json.JSONArray;
 import org.json.JSONObject;
 import java.util.Collections;
@@ -14,16 +15,15 @@ import java.util.Map;
 
 public class LeitorArquivo {
 
-  
     public static void main(String[] args) {
         try {
-            
+
             //lê o arquivo json
             Path path = Paths.get("C:\\Users\\manu\\Desktop\\Ordenacao\\src\\main\\java\\com\\mycompany\\ordenacao\\meu_json.json");
-            
+
             //converte o arquivo lido para uma string
             String conteudo = new String(Files.readAllBytes(path));
-            
+
             //converte a string em um objeto JSONArray usando a biblioteca JSON.
             JSONArray jsonArray = new JSONArray(conteudo);
 
@@ -33,20 +33,19 @@ public class LeitorArquivo {
             //percorre a lista para obter o valor "arq" para cada campo da lista
             for (int i = 0; i < jsonArray.length(); i++) {
                 JSONObject jsonObject = jsonArray.getJSONObject(i);
-                
+
                 //obtém o campo "arq" da lista
                 int arqValue = jsonObject.getInt("arq");
-                
+
                 //cria uma nova entrada map para o campo "arq" caso ele não exista
                 //na lista
                 if (!arqToObjectsMap.containsKey(arqValue)) {
                     arqToObjectsMap.put(arqValue, new ArrayList<>());
                 }
-                
+
                 //adiciona o objeto à lista correspondente no mapa
                 arqToObjectsMap.get(arqValue).add(jsonObject);
             }
-
 
             //percorre a lista afim de ordenar o campo "ordem" da lista
             for (int arqValue : arqToObjectsMap.keySet()) {
@@ -55,9 +54,9 @@ public class LeitorArquivo {
 
                 //ordena a lista de objetos comn abse no campo "ordem", utilizando o TimSort por de baixo dos panos
                 Collections.sort(objects, new Comparator<JSONObject>() {
-                    
+
                     //compara os avalores de "ordem" dos objetos para determinar a ordem de classificação
-                    @Override                    
+                    @Override
                     public int compare(JSONObject obj1, JSONObject obj2) {
                         return Integer.compare(obj1.getInt("ordem"), obj2.getInt("ordem"));
                     }
@@ -66,13 +65,16 @@ public class LeitorArquivo {
                 //utilizado para concatenar as notas de todos os objetos
                 StringBuilder todasAsNotas = new StringBuilder();
                 for (JSONObject obj : objects) {
-                    //concatena o campo "notas" de cada objeto
+                    // Imprime o campo "ordem" de cada objeto durante a ordenação
+                    //System.out.println("Ordem (arq=" + arqValue + "): " + obj.getInt("ordem"));
+
+                    // Concatena o campo "notas" de cada objeto
                     todasAsNotas.append(obj.getString("notas"));
                 }
 
                 String caminhoArquivoNotas = "C:\\Users\\manu\\Desktop\\Ordenacao\\src\\main\\java\\com\\mycompany\\ordenacao\\" + "arq_" + arqValue + ".json";
 
-                 //escreve o arquivo de saíoda
+                //escreve o arquivo de saíoda
                 try (FileWriter fileWriter = new FileWriter(caminhoArquivoNotas)) {
                     //escreve as notas concatenadas no arquivo.
                     fileWriter.write(todasAsNotas.toString());
